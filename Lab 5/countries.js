bb55194ee08243eaa62bdd3da4ff3b08b19a7496
Josué Quesada <51096767+josue-quesada@users.noreply.github.com>
2023-12-27 23:34:10 -0600

    update

--- Lab 5/countries.js
+++ Lab 5/countries.js
@@ -1,50 +1,132 @@
-function getAllCountries() {
-    var requestOptions = {
-        method: 'GET',
-        redirect: 'follow'
-      };
-      
-      fetch("https://restcountries.com/v3.1/all", requestOptions)
-        .then(response => response.text())
-        .then(result => console.log(JSON.parse(result)))
-        .catch(error => console.log('error', error));
-};
+$(document).ready(function () {
+  dataTableProperties();
+  });
 
-function getCountriesByName(){
+function getAllCountries() {
+  var requestOptions = {
+    method: "GET",
+    redirect: "follow",
+  };
+  clearDataTable();
+  fetch("https://restcountries.com/v3.1/all", requestOptions)
+    .then((response) => response.text())
+    .then((result) => {
+      var countries = JSON.parse(result);
+
+      var tbody = document.getElementById("table-body");
+      countries.forEach((country) => {
+        var tr = document.createElement("tr");
+        tr.innerHTML =
+          `<td>${country.name.official}</td>` +
+          `<td>${country.name.common}</td>` +
+          `<td>${country.translations.spa.common}</td>` +
+          `<td>${country.translations.spa.official}</td>` +
+          `<td>${
+            Array.isArray(country.capital) && country.capital.length > 0
+              ? country.capital[0]
+              : "-"
+          }</td>` +
+          `<td>${country.region}</td>` +
+          `<td>${'<button openBtn class = "action-btn" onclick="openModal()">Info</button>'}</td>`;
+        tbody.appendChild(tr);
+      });
+    });
+}
+
+function getCountriesByName() {
   var name = document.getElementById("search-input").value;
-  if(name === "" || name === undefined){
-    getAllCountries()
+  if (name === "" || name === undefined) {
+    getAllCountries();
     return;
   }
   document.getElementById("search-input").value = "";
-    var requestOptions = {
+  var requestOptions = {
+    method: "GET",
+    redirect: "follow",
+  };
+  clearDataTable();
+  var tbody = document.getElementById("table-body");
+  fetch("https://restcountries.com/v3.1/name/" + name, requestOptions)
+    .then((response) => response.text())
+    .then((result) => {
+      var countries = JSON.parse(result);
+      countries.forEach((country) => {
+        var tr = document.createElement("tr");
+        tr.innerHTML =
+          `<td>${country.name.official}</td>` +
+          `<td>${country.name.common}</td>` +
+          `<td>${country.translations.spa.common}</td>` +
+          `<td>${country.translations.spa.official}</td>` +
+          `<td>${
+            Array.isArray(country.capital) && country.capital.length > 0
+              ? country.capital[0]
+              : "-"
+          }</td>` +
+          `<td>${country.region}</td>` +
+          `<td>${'<button class = "action-btn">Info</button>'}</td>`;
+        tbody.appendChild(tr);
+      });
+    })
+    .catch((error) => console.log("error", error));
+}
+
+function getModalInfo(name) {
+  var requestOptions = {
     method: 'GET',
     redirect: 'follow'
   };
   
   fetch("https://restcountries.com/v3.1/name/" + name, requestOptions)
     .then(response => response.text())
-    .then(result => {
-      var countries = JSON.parse(result);
-      var tbody = document.getElementById("table-body");
-      countries.forEach(country => {
-        let row = tbody.insertRow();
-        let name = row.insertCell(0);
-        name.innerHTML = country.name.official;
-        let commonName = row.insertCell(1);
-        commonName.innerHTML = country.name.common;
-        let spanishName = row.insertCell(2);
-        spanishName.innerHTML = country.translations.spa.common;
-        let officialName = row.insertCell(3);
-        officialName.innerHTML = country.translations.spa.official;
-        let capital = row.insertCell(4);
-        capital.innerHTML = country.capital[0];
-        let region = row.insertCell(5);
-        region.innerHTML = country.region;
-        let button = row.insertCell(6);
-        button.innerHTML = '<button class = "action-btn">Acciones</button>';
-      });
-
-    })
+    .then(result => console.log(result))
     .catch(error => console.log('error', error));
+}
+
+
+function clearDataTable() {
+  var dataTable = $("#main-table").DataTable();
+  dataTable.clear().draw();
+}
+
+function dataTableProperties() {
+  $("#main-table").DataTable({
+    select: true,
+    bSort: true,
+    paging: true,
+    pageLength: 10,
+    lengthMenu: [10, 20, 30],
+
+    columns: [
+      { orderable: false },
+      { orderable: false },
+      { orderable: false },
+      { orderable: false },
+      { orderable: false },
+      { orderable: false },
+      { orderable: false },
+    ],
+    language: {
+      search: "Búsqueda:  ",
+      paginate: {
+        show: "Mostrando",
+        first: "Primer",
+        previous: "Anterior",
+        next: "Siguiente",
+        last: "Ultimo",
+      },
+      info: "Mostrando página _PAGE_  de _PAGES_",
+      infoEmpty: " ",
+      emptyTable: " ",
+    },
+  });
+}
+
+function openModal(){
+  const modal = document.querySelector("[modal]");
+  modal.showModal();
+}
+
+function closeModal(){
+  const modal = document.querySelector("[modal]");
+  modal.close();
 }
